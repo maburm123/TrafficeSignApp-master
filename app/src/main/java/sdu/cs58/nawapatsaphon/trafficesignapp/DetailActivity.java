0f78ead696da60f9ba9abf26ad6aa5e88c6ed776
maburm123 <maburm4848@gmail.com>
2018-04-04 10:47:35 +0700

    link complete

--- app/src/main/java/sdu/cs58/nawapatsaphon/trafficesignapp/DetailActivity.java
+++ app/src/main/java/sdu/cs58/nawapatsaphon/trafficesignapp/DetailActivity.java
@@ -2,6 +2,7 @@ package sdu.cs58.nawapatsaphon.trafficesignapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -24,4 +25,8 @@ public class DetailActivity extends AppCompatActivity {
         detailTextView.setText(getIntent().getStringExtra("Detail"));
         signImageView.setImageResource(getIntent().getIntExtra("Image",R.drawable.traffic_01));
     }// end onCreate
+
+    public void clickBack(View view) {
+        finish();
+    }//end clickback
 }// end class
